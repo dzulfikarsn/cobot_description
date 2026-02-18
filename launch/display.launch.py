7349f8c7d66deebbc7d20d4fc55e85f37c0deb85
Dzulfikar Saifuddin <dzulfikarsaifuddin@gmail.com>
2026-02-18 18:00:38 +0700

    make it simplier

--- launch/display.launch.py
+++ launch/display.launch.py
@@ -1,7 +1,5 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.conditions import IfCondition, UnlessCondition
-from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
+from launch.substitutions import Command, PathJoinSubstitution
 
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
@@ -13,44 +11,15 @@ def generate_launch_description():
     urdf_path = PathJoinSubstitution([package, 'urdf', 'cobot.urdf.xacro'])
     rviz_config = PathJoinSubstitution([package, 'rviz', 'urdf.rviz'])
 
-    # declare argument
-    argument_list = [
-        DeclareLaunchArgument(
-            name='jsp_gui',
-            default_value='true',
-            choices=['true', 'false'],
-            description='Flag to enable joint_state_publisher_gui'
-        ),
-        DeclareLaunchArgument(
-            name='urdf',
-            default_value=urdf_path,
-            description='Path to the URDF file'
-        ),
-        DeclareLaunchArgument(
-            name='rviz_cfg',
-            default_value=rviz_config,
-            description='Absolute path to rviz config file'
-        )
-    ]
-
-    # joint state publisher node
+    # joint state publisher gui node
     jsp_gui_node = Node(
         package='joint_state_publisher_gui',
         executable='joint_state_publisher_gui',
-        output='screen',
-        condition=IfCondition(LaunchConfiguration('jsp_gui'))
-    )
-
-    jsp_node = Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        output='screen',
-        condition=UnlessCondition(LaunchConfiguration('jsp_gui'))
+        output='screen'
     )
 
     # robot state publisher node
-    xacro_script = Command(['xacro ', LaunchConfiguration('urdf')])
-
+    xacro_script = Command(['xacro ', urdf_path])
     robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
@@ -65,15 +34,14 @@ def generate_launch_description():
         package='rviz2',
         executable='rviz2',
         output='screen',
-        arguments=['-d', LaunchConfiguration('rviz_cfg')]
+        arguments=['-d', rviz_config]
     )
 
     # add nodes
     node_list = [
         jsp_gui_node,
-        jsp_node,
         robot_state_publisher_node,
         rviz2_node,
     ]
 
-    return LaunchDescription(argument_list + node_list)
+    return LaunchDescription(node_list)
