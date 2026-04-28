63ed03e0b35c2b49f07f05fc6f83ff52023b381a
Dzulfikar Saifuddin <dzulfikarsaifuddin@gmail.com>
2026-04-28 11:31:44 +0700

    move rviz config to config dir

--- launch/display.launch.py
+++ launch/display.launch.py
@@ -9,7 +9,7 @@ from launch_ros.parameter_descriptions import ParameterValue
 def generate_launch_description():
     package = FindPackageShare('cobot_description')
     urdf_path = PathJoinSubstitution([package, 'urdf', 'cobot.urdf.xacro'])
-    rviz_config = PathJoinSubstitution([package, 'rviz', 'urdf.rviz'])
+    rviz_config = PathJoinSubstitution([package, 'config', 'urdf.rviz'])
 
     # joint state publisher gui node
     jsp_gui_node = Node(
